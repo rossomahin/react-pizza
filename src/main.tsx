5a3d7fc7835d3577dc57b24522746716e46684b0
Dmitry Rossomahin <rossomahin@rossomahin-2.local>
2026-01-15 23:35:12 +0300

    Replace BrowseRouter on HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
@@ -10,12 +10,12 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/react-pizza">
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route index element={<App />} />
+        <Route path="cart" element={<Cart />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
